286b049b15e66ab5cc057d67f1e1a8d8e8762fcb
Logan Hasson <logan.hasson@gmail.com>
2014-01-16 11:43:18 -0500

    Re-comment out bonus question specs

--- spec/hashketball_spec.rb
+++ spec/hashketball_spec.rb
@@ -246,42 +246,42 @@ end
 
 # If you'd like to work on the bonus, uncomment these tests.
 
-describe 'bonus' do
+# describe 'bonus' do
 
-  describe '#most_points_scored' do
+#   describe '#most_points_scored' do
 
-    it 'returns Ben Gordon' do
-      expect(most_points_scored).to eq("Ben Gordon")
-    end
+#     it 'returns Ben Gordon' do
+#       expect(most_points_scored).to eq("Ben Gordon")
+#     end
 
-  end
+#   end
 
-  describe '#winning_team' do
+#   describe '#winning_team' do
 
-    it 'returns the Brooklyn Nets' do
-      expect(winning_team).to eq("Brooklyn Nets")
-    end
+#     it 'returns the Brooklyn Nets' do
+#       expect(winning_team).to eq("Brooklyn Nets")
+#     end
 
-  end
+#   end
 
-  describe '#player_with_longest_name' do
+#   describe '#player_with_longest_name' do
 
-    it 'returns Brendan Haywood' do
-      expect(player_with_longest_name).to eq("Brendan Haywood")
-    end
+#     it 'returns Brendan Haywood' do
+#       expect(player_with_longest_name).to eq("Brendan Haywood")
+#     end
 
-  end
+#   end
 
-end
+# end
 
-describe 'super bonus' do
+# describe 'super bonus' do
 
-  describe '#long_name_steals_a_ton?' do
+#   describe '#long_name_steals_a_ton?' do
 
-    it 'returns true' do
-      expect(long_name_steals_a_ton?).to eq(true)
-    end
+#     it 'returns true' do
+#       expect(long_name_steals_a_ton?).to eq(true)
+#     end
 
-  end
+#   end
 
-end
+# end
