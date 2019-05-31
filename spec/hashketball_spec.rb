f317a8293e008070ebcaaf0367c2e5f59574e91b
maxwellbenton <maxwellbenton@gmail.com>
2019-05-31 09:56:59 -0400

    updates stats to match super bonus

--- spec/hashketball_spec.rb
+++ spec/hashketball_spec.rb
@@ -111,7 +111,7 @@ describe 'hashketball' do
         :points => 12,
         :rebounds => 4,
         :assists => 7,
-        :steals => 7,
+        :steals => 22,
         :blocks => 15,
         :slam_dunks => 10
       }
@@ -150,7 +150,7 @@ describe 'hashketball' do
         :points => 6,
         :rebounds => 12,
         :assists => 12,
-        :steals => 22,
+        :steals => 7,
         :blocks => 5,
         :slam_dunks => 12
       }
@@ -248,42 +248,42 @@ end
 
 # If you'd like to work on the bonus, uncomment these tests.
 
-# describe 'bonus' do
+describe 'bonus' do
 
-#   describe '#most_points_scored' do
+  describe '#most_points_scored' do
 
-#     it 'returns Ben Gordon' do
-#       expect(most_points_scored).to eq("Ben Gordon")
-#     end
+    it 'returns Ben Gordon' do
+      expect(most_points_scored).to eq("Ben Gordon")
+    end
 
-#   end
+  end
 
-#   describe '#winning_team' do
+  describe '#winning_team' do
 
-#     it 'returns the Brooklyn Nets' do
-#       expect(winning_team).to eq("Brooklyn Nets")
-#     end
+    it 'returns the Brooklyn Nets' do
+      expect(winning_team).to eq("Brooklyn Nets")
+    end
 
-#   end
+  end
 
-#   describe '#player_with_longest_name' do
+  describe '#player_with_longest_name' do
 
-#     it 'returns Bismack Biyombo' do
-#       expect(player_with_longest_name).to eq("Bismack Biyombo")
-#     end
+    it 'returns Bismack Biyombo' do
+      expect(player_with_longest_name).to eq("Bismack Biyombo")
+    end
 
-#   end
+  end
 
-# end
+end
 
-# describe 'super bonus' do
+describe 'super bonus' do
 
-#   describe '#long_name_steals_a_ton?' do
+  describe '#long_name_steals_a_ton?' do
 
-#     it 'returns true' do
-#       expect(long_name_steals_a_ton?).to eq(true)
-#     end
+    it 'returns true' do
+      expect(long_name_steals_a_ton?).to eq(true)
+    end
 
-#   end
+  end
 
-# end
+end
