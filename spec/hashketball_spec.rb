51988083a785c890bbf9448a1a22b55b878f9258
maxwellbenton <maxwellbenton@gmail.com>
2019-06-10 12:36:58 -0400

    updated player rebound stat for big_shoe_rebound

--- spec/hashketball_spec.rb
+++ spec/hashketball_spec.rb
@@ -200,7 +200,7 @@ describe 'hashketball' do
         :number => 1,
         :shoe => 19,
         :points => 26,
-        :rebounds => 12,
+        :rebounds => 11,
         :assists => 6,
         :steals => 3,
         :blocks => 8,
@@ -239,7 +239,7 @@ describe 'hashketball' do
   describe '#big_shoe_rebounds' do
 
     it 'returns the number of rebounds of the player with the biggest shoe size' do
-      expect(big_shoe_rebounds).to eq(12)
+      expect(big_shoe_rebounds).to eq(11)
     end
 
   end
